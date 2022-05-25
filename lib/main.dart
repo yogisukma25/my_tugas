480d60f943fc8a0bee156354d05a25a736bd181a
FlutLab.io <>
2022-05-25 03:50:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -49,13 +49,13 @@ class Tugas extends StatelessWidget {
                             ),
                             borderRadius: BorderRadius.circular(12),
                           ),
-                          child: Card(
-                            child: ListTile(
-                              title: Text("Xiaomi Note 9"),
-                              subtitle: Text("Rp 2.250.000,00"),
-                            ),
+                        ),
+                        Card(
+                          child: ListTile(
+                            title: Text("Xiaomi Note 9"),
+                            subtitle: Text("Rp 2.250.000,00"),
                           ),
-                        )
+                        ),
                       ],
                     )
                   ],
