2c563cda509673ad023df7010f4586646db9be5e
FlutLab.io <>
2022-05-25 04:40:21 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class Tugas extends StatelessWidget {
                             borderRadius: BorderRadius.circular(12),
                           ),
                           child: Container(
-                            margin: EdgeInsets.fromLTRB(0, 0, 200, 0),
+                            margin: EdgeInsets.fromLTRB(0, 0, 0, 200),
                             color: Colors.white,
                             child: ListTile(
                               title: Text(
