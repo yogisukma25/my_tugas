3082982b5eee85c8e3b293b68b3f40f7645c9304
FlutLab.io <>
2022-05-25 04:18:10 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -180,6 +180,114 @@ class Tugas extends StatelessWidget {
                             ),
                           ),
                         ),
+                        Container(
+                          height: 200,
+                          width: 155,
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: const Color(0xff7c94b6),
+                            image: const DecorationImage(
+                              image: NetworkImage('https://i.postimg.cc/Bn9xCv9B/1-2.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                            border: Border.all(
+                              color: Colors.green,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                            color: Colors.white,
+                            child: ListTile(
+                              title: Text(
+                                "Xiaomi Note 9",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text(
+                                "Rp 2.250.000,00",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 200,
+                          width: 155,
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: const Color(0xff7c94b6),
+                            image: const DecorationImage(
+                              image: NetworkImage('https://i.postimg.cc/Bn9xCv9B/1-2.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                            border: Border.all(
+                              color: Colors.green,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                            color: Colors.white,
+                            child: ListTile(
+                              title: Text(
+                                "Xiaomi Note 9",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text(
+                                "Rp 2.250.000,00",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 200,
+                          width: 155,
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: const Color(0xff7c94b6),
+                            image: const DecorationImage(
+                              image: NetworkImage('https://i.postimg.cc/Bn9xCv9B/1-2.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                            border: Border.all(
+                              color: Colors.green,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                            color: Colors.white,
+                            child: ListTile(
+                              title: Text(
+                                "Xiaomi Note 9",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text(
+                                "Rp 2.250.000,00",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     )
                   ],
